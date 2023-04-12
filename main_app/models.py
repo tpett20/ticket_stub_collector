03c64146945c0bca6d864e87583ede2cfc39e534
tpett20 <thomaspettersen20@gmail.com>
2023-04-12 14:02:32 -0400

    correct Ticket model

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Ticket(models.Model):
-    event: models.CharField(max_length=150)
-    highlight: models.TextField
-    date: models.DateField
-    venue: models.CharField(max_length=150)
+    event = models.CharField(max_length=150)
+    highlight = models.TextField
+    date = models.DateField
+    venue = models.CharField(max_length=150)
 
     def __str__(self):
         return self.event
