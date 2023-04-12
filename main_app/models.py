4d42c34a092e4034bec70cca203f70a113ab776f
tpett20 <thomaspettersen20@gmail.com>
2023-04-12 13:47:36 -0400

    create Ticket model.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    event: models.CharField(max_length=150)
+    highlight: models.TextField
+    date: models.DateField
+    venue: models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.event
