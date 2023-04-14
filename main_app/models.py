7504a32ebeaf36aca1d52fbc8664f77b9744a186
tpett20 <thomaspettersen20@gmail.com>
2023-04-14 14:26:48 -0400

    add comment for one to many ticket to comment relationship

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+# class Companion(models.Model):
+#     name = models.CharField(max_length=75)
+#     relation = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return self.name
+
 class Ticket(models.Model):
     event = models.CharField(max_length=150)
     description = models.TextField(max_length=250)
@@ -14,5 +21,17 @@ class Ticket(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'ticket_id': self.id})
     
+    class Meta:
+        ordering = ['-date']
+
+class Comment(models.Model):
+    date = models.DateField(auto_now_add=True)
+    author = models.CharField(max_length=75)
+    content = models.TextField('comment')
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} on {self.date}"
+
     class Meta:
         ordering = ['-date']
